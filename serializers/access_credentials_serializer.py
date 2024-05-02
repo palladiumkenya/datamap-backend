4ff8f2107c2e7e660c8164c8da604574c28bba8b
cbrianbet <cbrianbet@outlook.com>
2024-05-02 19:26:06 +0300

    added fields to access_credentials

--- serializers/access_credentials_serializer.py
+++ serializers/access_credentials_serializer.py
@@ -2,8 +2,11 @@ def access_credential_entity(credential) -> dict:
     return {
         "id": str(credential["id"]),
         "conn_string": str(credential["conn_string"]),
+        "name": str(credential["name"]),
+        "system": str(credential["system"]),
         "is_active": bool(credential["is_active"]),
-        "created_at": credential["created_at"]
+        "created_at": credential["created_at"],
+        "updated_at": credential["updated_at"]
     }
 
 
