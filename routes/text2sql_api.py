29387e2ab1264a4446d8db6a72718ddbbd70edcd
NewtonMutugi <newtonmut197@gmail.com>
2024-07-15 10:36:50 +0300

    chore: Update text2sql_api.py with new changes

--- routes/text2sql_api.py
+++ routes/text2sql_api.py
@@ -149,89 +149,86 @@ async def query_from_natural_language(nl_query: NaturalLanguageQuery):
             obj_index.as_retriever(similarity_top_k=2),
         )
         custom_txt2sql_prompt = """Given an input question, construct a syntactically correct SQL query to run, then look at the results of the query and return a comprehensive and detailed answer. Ensure that you:
-                    - Select only the relevant columns needed to answer the question.
-                    - Use correct column and table names as provided in the schema description. Avoid querying for columns that do not exist.
-                    - Qualify column names with the table name when necessary, especially when performing joins.
-                    - Use aggregate functions appropriately and include performance optimizations such as WHERE clauses and indices.
-                    - Add additional related information for the user.
-                    - Use background & definitions provided for more detailed answer. Follow the instructions.
-                    - Your are provided with several tables each for a different proram area, ensure you retrive the relevant table.
-                    - do not hallucinate column names. If you can't  find a column name, do not write the sql query say I'm not sure.
+            - Select only the relevant columns needed to answer the question.
+            - Use correct column and table names as provided in the schema description. Avoid querying for columns that do not exist.
+            - Qualify column names with the table name when necessary, especially when performing joins.
+            - Use aggregate functions appropriately and include performance optimizations such as WHERE clauses and indices.
+            - Add additional related information for the user.
+            - Use background & definitions provided for more detailed answer. Follow the instructions.
+            - Your are provided with several tables each for a different proram area, ensure you retrive the relevant table.
+            - do not hallucinate column names. If you can't  find a column name, do not write the sql query say I'm not sure.
 
-                    When answering questions that include the string "PreP," utilize the LinelistPrepAssessments table to construct the SQL code.
-                    When answering questions that include the string "OVC" utilize the  LineListOVCEligibilityAndEnrollments table to construct the SQL code.
-                    When answering questions that include the string "OTZ" utilize the  LineListOTZEligibilityAndEnrollments table to construct the SQL code.
-                    When answering questions that include the string " HEI or infants" utilize the  LinelistHEI table to construct the SQL code.
-                    When answering questions that include the string "PBFW," utilize the LineListPBFW table to construct the SQL code.
-                    When answering questions that include the string "HTS" or "HIV tests"  utilize the LineListTransHTS table to construct the SQL code.
-
-
-                    Special Instructions:
-                    - Treat "txcurr" and "active patients on treatment" as interchangeable terms in your queries.
-                    - Exposed Infants (HEI) and HEI are used interchangably.
-                    - Default to using averages for aggregation if not specified by the user question.
-                    - If the requested date range is not available in the database, inform the user that data is not available for that time period.
-                    - Use bold and large fonts to highlight keywords in your answer.
-                    - If the date is not available, your answer will be: Data is not available for the requested date range. Please modify your query to include a valid date range.
-                    - Calculate date ranges dynamically based on the current date or specific dates mentioned in user queries. Use relative time expressions such as "last month" or "past year".
-                    - If a query fails to execute, suggest debugging tips or provide alternative queries. Ensure to handle common SQL errors gracefully."
-                    - If the query is ambiguous, generate a clarifying question to better understand the user's intent or request additional necessary parameters.
-                    - Use indexed columns for joins and WHERE clauses to speed up query execution. Use `EXPLAIN` plans for complex queries to ensure optimal performance.
-                    - Join ON "PatientPKHASH" AND "MFLCode" for queries that require joins.
-                    - txcurr is where IsTxcurr=1. "
-                    - When NUPI = NULL means you have not been verified"
-                    - Seroconversion is turning Positive from HIV test
-                    - Interruption in treatment (IIT) is the patients with Loss to Follow up outcome description."
-                    - Pre-exposure prophylaxis and PreP are used interchangably
-
-                    Additional Instructions:
-                    Please confirm the variables names in the schema before generating a query
-
-                    You are required to use the following format, each taking one line:
-                    Question: Question here
-                    SQLQuery: SQL Query to run
+             When answering questions that include the string "PreP," utilize the LinelistPrepAssessments table to construct the SQL code.
+             When answering questions that include the string "OVC" utilize the  LineListOVCEligibilityAndEnrollments table to construct the SQL code.
+             When answering questions that include the string "OTZ" utilize the  LineListOTZEligibilityAndEnrollments table to construct the SQL code.
+             When answering questions that include the string " HEI or infants" utilize the  LinelistHEI table to construct the SQL code.
+             When answering questions that include the string "PBFW," utilize the LineListPBFW table to construct the SQL code.
+             When answering questions that include the string "HTS" or "HIV tests"  utilize the LineListTransHTS table to construct the SQL code.
 
 
-                    The text-to-SQL system that might be required to handle queries related to calculating proportions within a dataset. Your system should be able to generate SQL queries to calculate the proportion of a certain category within a dataset table.
+             Special Instructions:
+            - Treat "txcurr" and "active patients on treatment" as interchangeable terms in your queries.
+            - Exposed Infants (HEI) and HEI are used interchangably.
+            - Default to using averages for aggregation if not specified by the user question.
+            - If the requested date range is not available in the database, inform the user that data is not available for that time period.
+            - Use bold and large fonts to highlight keywords in your answer.
+            - If the date is not available, your answer will be: Data is not available for the requested date range. Please modify your query to include a valid date range.
+            - Calculate date ranges dynamically based on the current date or specific dates mentioned in user queries. Use relative time expressions such as "last month" or "past year".
+            - If a query fails to execute, suggest debugging tips or provide alternative queries. Ensure to handle common SQL errors gracefully."
+            - If the query is ambiguous, generate a clarifying question to better understand the user's intent or request additional necessary parameters.
+            - Use indexed columns for joins and WHERE clauses to speed up query execution. Use `EXPLAIN` plans for complex queries to ensure optimal performance.
+            - Join ON "PatientPKHASH" AND "MFLCode" for queries that require joins.
+            - txcurr is where IsTxcurr=1. "
+            - When NUPI = NULL means you have not been verified"
+            - Seroconversion is turning Positive from HIV test
+            - Interruption in treatment (IIT) is the patients with Loss to Follow up outcome description."
+            - Pre-exposure prophylaxis and PreP are used interchangably
+            - Infected Infants also means HEI
 
-                    Example 1 :
-                    If a user asks, "What proportion of TxCurr,  have hypertension by county", your system should generate a SQL query like:
+             Additional Instructions:
+            Please confirm the variables names in the schema before generating a query
 
-
-                    SELECT County, COUNT(*) AS TotalTxCurr, SUM(CASE WHEN HasHypertension = 1 THEN 1 ELSE 0 END) AS TotalHypertension, SUM(CASE WHEN HasHypertension = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS ProportionHypertension FROM Linelist_FACTART WHERE ISTxCurr = 1 GROUP BY County;
-
-                    Example 2:
-                    If a user asks, "What is the proportion of clients screened for Prep per county", your system should generate a SQL query like:
-
-                    SELECT County, COUNT(*) AS ScreenedPrep, SUM(CASE WHEN ScreenedPrep = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS ProportionScreenedForPrep FROM LinelistPrepAssessments GROUP BY County;
-
-
-                    Example 3 :
-                    If a user asks, "What is unsuppression rate of all active clients on treatment by county", your system should generate a SQL query like:
-
-                    SELECT County, COUNT(*) AS TotalPatientsWithValidVL, COUNT(CASE WHEN HighViremia = 1 THEN 1 END) AS UnsuppressedPatients, COUNT(CASE WHEN HighViremia = 1 THEN 1 END) * 100.0 / COUNT(*) AS UnsuppressionRate FROM Linelist_FACTART WHERE ISTxCurr = 1 AND HasValidVL = 1 GROUP BY County;
-
-                    The text-to-SQL system that might be required to handle queries related to calculating trends or rate of change. Your system should be able to generate SQL queries to calculate trend and increase rate.
-
-                    Instructions:
-
-                    Calculate the rates :Compute Rate Differences: Calculate change in rate(subtract the rate at end period-start period/start period): Multiply the rate difference by 100 to express it as a percentage.
-
-                    Example 1 :
-                    If a user asks, "What is the trend in positivity rates across different counties from January 2023 to June 2023, and which county experienced the greatest increase in positivity rate over this period?"", your system should generate a SQL query like:
-
-                    SELECT County, MAX(CASE WHEN Month = 1 THEN PositivityRate END) AS PositivityRate_Month1, MAX(CASE WHEN Month = 2 THEN PositivityRate END) AS PositivityRate_Month2, MAX(CASE WHEN Month = 3 THEN PositivityRate END) AS PositivityRate_Month3, MAX(CASE WHEN Month = 4 THEN PositivityRate END) AS PositivityRate_Month4, MAX(CASE WHEN Month = 5 THEN PositivityRate END) AS PositivityRate_Month5, MAX(CASE WHEN Month = 6 THEN PositivityRate END) AS PositivityRate_Month6, (MAX(CASE WHEN Month = 6 THEN PositivityRate END) - MAX(CASE WHEN Month = 1 THEN PositivityRate END)) AS PositivityRateDifference, CASE WHEN MAX(CASE WHEN Month = 1 THEN PositivityRate END) <> 0 THEN (((MAX(CASE WHEN Month = 6 THEN PositivityRate END) - MAX(CASE WHEN Month = 1 THEN PositivityRate END)) / MAX(CASE WHEN Month = 1 THEN PositivityRate END)) * 100) ELSE NULL END AS RateDifference FROM ( SELECT County, MONTH(TestDate) AS Month, COUNT(CASE WHEN FinalTestResult = 'Positive' THEN 1 END) AS PositiveTests, COUNT(*) AS TotalTests, CASE WHEN COUNT(*) <> 0 THEN COUNT(CASE WHEN FinalTestResult = 'Positive' THEN 1 END) * 100.0 / COUNT(*) ELSE NULL END AS PositivityRate FROM LineListTransHTS WHERE TestDate >= '2023-01-01' AND TestDate < '2023-07-01' GROUP BY County, MONTH(TestDate) ) AS PositivityRates GROUP BY County;
-                    when asked for a specific county, Partner or facility , order in descending and filter to the top county/partner/facility, otherwise provide a linelist
-
-                    The text-to-SQL system that might be required to handle queries related to joining different tablea. Your system should be able to generate SQL queries to joins different tables and selects the variables needed.
-                    Example 1 :
-                    If a user asks, "among Counties that conducted more than 10,000 HIV tests in 2023, which county has the highest number of active patients on treatment?, your system should generate a SQL query like:
-
-                    SQLQUERY: SELECT County, COUNT(*) AS TotalTxCurr FROM Linelist_FACTART WHERE County IN (SELECT County FROM LineListTransHTS WHERE YEAR(TestDate) = 2023 GROUP BY County HAVING COUNT(*) > 10000) AND ISTxCurr = 1 GROUP BY County ORDER BY TotalTxCurr DESC;
+            You are required to use the following format, each taking one line:
+            Question: Question here
+            SQLQuery: SQL Query to run
 
 
+            The text-to-SQL system that might be required to handle queries related to calculating proportions within a dataset. Your system should be able to generate SQL queries to calculate the proportion of a certain category within a dataset table.
+
+            Example 1 :
+            If a user asks, "What proportion of TxCurr,  have hypertension by county", your system should generate a SQL query like:
+            hints only gives you the columns, please use the hint to calculate proportions
+
+            SELECT County, COUNT(*) AS TotalTxCurr, SUM(CASE WHEN HasHypertension = 1 THEN 1 ELSE 0 END) AS TotalHypertension, SUM(CASE WHEN HasHypertension = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS ProportionHypertension FROM Linelist_FACTART WHERE ISTxCurr = 1 GROUP BY County;
+
+            Example 2:
+            If a user asks, "What is the proportion of clients screened for Prep per county", your system should generate a SQL query like:
+
+            SELECT County, COUNT(*) AS ScreenedPrep, SUM(CASE WHEN ScreenedPrep = 1 THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS ProportionScreenedForPrep FROM LinelistPrepAssessments GROUP BY County;
 
 
+            Example 3 :
+            If a user asks, "What is unsuppression rate of all active clients on treatment by county", your system should generate a SQL query like:
+
+            SELECT County, COUNT(*) AS TotalPatientsWithValidVL, COUNT(CASE WHEN HighViremia = 1 THEN 1 END) AS UnsuppressedPatients, COUNT(CASE WHEN HighViremia = 1 THEN 1 END) * 100.0 / COUNT(*) AS UnsuppressionRate FROM Linelist_FACTART WHERE ISTxCurr = 1 AND HasValidVL = 1 GROUP BY County;
+
+            The text-to-SQL system that might be required to handle queries related to calculating trends or rate of change. Your system should be able to generate SQL queries to calculate trend and increase rate.
+
+            Instructions:
+
+            Calculate the rates :Compute Rate Differences: Calculate change in rate(subtract the rate at end period-start period/start period): Multiply the rate difference by 100 to express it as a percentage.
+
+            Example 1 :
+            If a user asks, "What is the trend in positivity rates across different counties from January 2023 to June 2023, and which county experienced the greatest increase in positivity rate over this period?"", your system should generate a SQL query like:
+
+            SELECT County, MAX(CASE WHEN Month = 1 THEN PositivityRate END) AS PositivityRate_Month1, MAX(CASE WHEN Month = 2 THEN PositivityRate END) AS PositivityRate_Month2, MAX(CASE WHEN Month = 3 THEN PositivityRate END) AS PositivityRate_Month3, MAX(CASE WHEN Month = 4 THEN PositivityRate END) AS PositivityRate_Month4, MAX(CASE WHEN Month = 5 THEN PositivityRate END) AS PositivityRate_Month5, MAX(CASE WHEN Month = 6 THEN PositivityRate END) AS PositivityRate_Month6, (MAX(CASE WHEN Month = 6 THEN PositivityRate END) - MAX(CASE WHEN Month = 1 THEN PositivityRate END)) AS PositivityRateDifference, CASE WHEN MAX(CASE WHEN Month = 1 THEN PositivityRate END) <> 0 THEN (((MAX(CASE WHEN Month = 6 THEN PositivityRate END) - MAX(CASE WHEN Month = 1 THEN PositivityRate END)) / MAX(CASE WHEN Month = 1 THEN PositivityRate END)) * 100) ELSE NULL END AS RateDifference FROM ( SELECT County, MONTH(TestDate) AS Month, COUNT(CASE WHEN FinalTestResult = 'Positive' THEN 1 END) AS PositiveTests, COUNT(*) AS TotalTests, CASE WHEN COUNT(*) <> 0 THEN COUNT(CASE WHEN FinalTestResult = 'Positive' THEN 1 END) * 100.0 / COUNT(*) ELSE NULL END AS PositivityRate FROM LineListTransHTS WHERE TestDate >= '2023-01-01' AND TestDate < '2023-07-01' GROUP BY County, MONTH(TestDate) ) AS PositivityRates GROUP BY County;
+            when asked for a specific county, Partner or facility , order in descending and filter to the top county/partner/facility, otherwise provide a linelist
+
+            The text-to-SQL system that might be required to handle queries related to joining different tablea. Your system should be able to generate SQL queries to joins different tables and selects the variables needed.
+            Example 1 :
+            If a user asks, "among Counties that conducted more than 10,000 HIV tests in 2023, which county has the highest number of active patients on treatment?, your system should generate a SQL query like:
+
+            SQLQUERY: SELECT County, COUNT(*) AS TotalTxCurr FROM Linelist_FACTART WHERE County IN (SELECT County FROM LineListTransHTS WHERE YEAR(TestDate) = 2023 GROUP BY County HAVING COUNT(*) > 10000) AND ISTxCurr = 1 GROUP BY County ORDER BY TotalTxCurr DESC;
                 """
 
         from llama_index.core.retrievers import NLSQLRetriever
@@ -252,13 +249,14 @@ async def query_from_natural_language(nl_query: NaturalLanguageQuery):
         print("First Identified Table:", first_identified_table)
         print("Second Identified Table:", second_identified_table)
 
-        custom_prompt_1 = (f"You can refer to {custom_txt2sql_prompt} for examples and instructions on how to generate a SQL statement."
+        custom_prompt_1 = ("Please calculate proportion when asked to, generate sql query that contains both the numbers and proportion. Only output sql query, do not attempt to generate an answer"
+                           f"You can refer to {custom_txt2sql_prompt} for examples and instructions on how to generate a SQL statement."
                            f"Write a SQL query to answer the following question: {question}. "
                            f"Using the table {first_identified_table}."
-                           "custom_prompt Please take note of the column names which are in quotes and their description."
+                           "Please take note of the column names which are in quotes and their description."
                            )
-
-        custom_prompt_2 = (f"You can refer to {custom_txt2sql_prompt} for examples and instructions on how to generate a SQL statement. "
+        custom_prompt_2 = ("Please calculate proportion when asked to, generate sql query that contains both the numbers and proportion. Only output sql query, do not attempt to generate an answer"
+                           f"You can refer to {custom_txt2sql_prompt} for examples and instructions on how to generate a SQL statement. "
                            f"Write a SQL query to answer the following question: {question}, using the table {first_identified_table}. "
                            "Please take note of the column names which are in quotes and their description. Do not use the two tables if you are not merging, be careful to differentiate which column names are in which table."
                            f"If the question requires joining or merging, join with {second_identified_table} to retrieve the required variables."
