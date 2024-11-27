ba602c43fc808b0ff9cc8c51be6ae038ab66ec56
MaryKilewe <marykilewe@gmail.com>
2024-11-27 14:54:57 +0300

    added delete site config and updated add site config logic

--- routes/site_configuration_api.py
+++ routes/site_configuration_api.py
@@ -32,7 +32,7 @@ def get_active_site_config():
             response_config = config
         else:
             response_config = None
-        return response_config
+        return {"data": response_config}
     except SiteConfig.DoesNotExist:
         raise HTTPException(status_code=404, detail="Config not found")
     except Exception as e:
@@ -43,24 +43,62 @@ class SaveSiteConfig(BaseModel):
     site_name: str = Field(..., description="")
     site_code: str = Field(..., description="")
     primary_system: str = Field(..., description="")
+    is_active: bool = Field(..., description="")
 
 
 @router.post("/add_site_config")
 def add_site_config(data: SaveSiteConfig):
     try:
+        if data.is_active:
+            site_configs = SiteConfig.objects.all()
+            if site_configs is not None:
+                for config in site_configs:
+                    config.is_active = False
+                    config.save()
+
+        site_config = SiteConfig(
+            site_name=data.site_name,
+            site_code=data.site_code,
+            primary_system=data.primary_system,
+            is_active=data.is_active
+        )
+        site_config.save()
+
+        return {"status": "success", "data": site_config}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+
+@router.put("/edit_site_config")
+def edit_site_config(data: SaveSiteConfig):
+    try:
+        if data.is_active:
+            site_configs = SiteConfig.objects.all()
+            if site_configs is not None:
+                for config in site_configs:
+                    config.is_active = False
+                    config.save()
+
         site_config = SiteConfig.objects.first()
         if site_config is not None:
             site_config.site_name = data.site_name
             site_config.site_code = data.site_code
             site_config.primary_system = data.primary_system
+            site_config.is_active = data.is_active
+
             site_config.save()
-        else:
-            site_config = SiteConfig(
-                site_name=data.site_name,
-                site_code=data.site_code,
-                primary_system=data.primary_system
-            )
-            site_config.save()
+
+        return {"status": "success", "data": site_config}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.delete("/delete_site_config/{config_id}")
+def delete_site_config(config_id: str):
+    try:
+        site_config = SiteConfig.objects(id=config_id).delete()
+
         return {"status": "success", "data": site_config}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
