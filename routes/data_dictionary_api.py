fbc73471d0b4fea1480182a93e5dfd65ccee49da
MaryKilewe <marykilewe@gmail.com>
2025-04-08 22:48:18 +0300

    separated extraction api and source_system db engine creation

--- routes/data_dictionary_api.py
+++ routes/data_dictionary_api.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 
 import requests
 from fastapi import APIRouter, HTTPException, BackgroundTasks, Depends
-from sqlalchemy import Column, UUID, DateTime, String, Integer, Boolean, Float, Double, MetaData, Table
+from sqlalchemy import Column, UUID, DateTime, String, Integer, Boolean, Float, Double, MetaData, Table, inspect
 from sqlalchemy.orm import Session
 
 from database.database import get_db, engine
@@ -191,7 +191,7 @@ def create_tables(db):
         # Create dynamic table class and synchronize with PSQL
         dynamic_table = Table(table_name, metadata, *columns_list)
 
-        if engine.dialect.has_table(engine, table_name):
+        if inspect(engine).has_table(table_name):
             dynamic_table.drop(engine)
         metadata.create_all(engine)
 
